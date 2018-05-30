226a865ede034295f360b6f0339521369ddf960b
Zach Breit <zbreit18@gmail.com>
2018-05-30 12:28:08 -0400

    Combined the multiple date objects into a single dictionary

--- decadesDataWrapper.py
+++ decadesDataWrapper.py
@@ -9,35 +9,49 @@ decades_of_music_df = pd.read_csv('top100ByDecade.csv', encoding='latin-1')
 
 # Store only the artist name and title of the top 100 songs from 1950-2010
 decades_of_music = {
-    '1950s': decades_of_music_df[500:600][['artist_name', 'title']],
-    '1960s': decades_of_music_df[400:500][['artist_name', 'title']],
-    '1970s': decades_of_music_df[300:400][['artist_name', 'title']],
-    '1980s': decades_of_music_df[200:300][['artist_name', 'title']],
-    '1990s': decades_of_music_df[100:200][['artist_name', 'title']],
-    '2000s': decades_of_music_df[0:100][['artist_name', 'title']]
-}
-
-# Create a placeholder object to store all of the lyrics for top 100 songs in each decade
-lyrics_by_decade = {
-    '1950s': [],
-    '1960s': [],
-    '1970s': [],
-    '1980s': [],
-    '1990s': [],
-    '2000s': []
+    '1950s': {
+        'dataframe': decades_of_music_df[500:600][['artist_name', 'title']],
+        'lyrics': [],
+        'song_sentiments': {}
+    },
+    '1960s': {
+        'dataframe': decades_of_music_df[400:500][['artist_name', 'title']],
+        'lyrics': [],
+        'song_sentiments': {}
+    },
+    '1970s': {
+        'dataframe': decades_of_music_df[300:400][['artist_name', 'title']],
+        'lyrics': [],
+        'song_sentiments': {}
+    },
+    '1980s': {
+        'dataframe': decades_of_music_df[200:300][['artist_name', 'title']],
+        'lyrics': [],
+        'song_sentiments': {}
+    },
+    '1990s': {
+        'dataframe': decades_of_music_df[100:200][['artist_name', 'title']],
+        'lyrics': [],
+        'song_sentiments': {}
+    },
+    '2000s': {
+        'dataframe': decades_of_music_df[0:100][['artist_name', 'title']],
+        'lyrics': [],
+        'song_sentiments': {}
+    }
 }
 
 number_of_songs = 1
 
 # For each decade, iterate over all of the songs and search for lyrics
-for decade, df in decades_of_music.items():
-    for index, row in df.iterrows():
+for decade, dictionary in decades_of_music.items():
+    for index, row in dictionary['dataframe'].iterrows():
         print(number_of_songs, ': ', end='')
         try:
             # Append each information about a given song to the given decade's lyrics array
             lyrics = getLyrics(row['title'], row['artist_name'])
 
-            lyrics_by_decade[decade].append({
+            decades_of_music[decade]['lyrics'].append({
                 'artist_name': row['artist_name'],
                 'song_title': row['title'],
                 'lyrics': lyrics,
@@ -56,20 +70,11 @@ for decade, df in decades_of_music.items():
                 row['artist_name'], get_exception_string(e)))
         number_of_songs += 1
 
-song_sentiments = {
-    '1950s': {},
-    '1960s': {},
-    '1970s': {},
-    '1980s': {},
-    '1990s': {},
-    '2000s': {}
-}
-
-for dictionary in song_sentiments:
+for decade, dictionary in decades_of_music.items():
     for key in ['positive_count', 'negative_count', 'neutral_count']:
-        dictionary[key] = 0
+        dictionary['song_sentiments'][key] = 0
 
-for decade, song_list in lyrics_by_decade.items():
+for decade, dictionary in decades_of_music.items():
     for song_info in song_list:
         if song_info['lyrics_polarity'] > 0:
             song_sentiments[decade]['positive_count'] += 1
